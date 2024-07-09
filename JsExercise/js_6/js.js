cfebecdb5656297e95a41a325eeb874b1c47bba0
Mario Tolvo <mariobros9@hotmail.it>
2024-07-09 18:24:27 +0200

    Inserire un numero

---
+++ JsExercise/js_6/js.js
@@ -0,0 +1,13 @@
+let score = parseInt(prompt("Inserire un numero"));
+
+if(score >= 90 && score <= 100){
+    console.log("voto ottimo");
+} else if(score >= 70 && score <= 89){
+    console.log("voto buono");
+} else if (score >= 60 && score <= 69){
+    console.log("voto sufficiente");
+} else if (score <= 59){
+    console.log("voto insufficiente");
+} else {
+    console.log("punteggio non valido");
+}
