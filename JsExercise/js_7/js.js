a96a4a936db2227213f640e76885384c60ce8344
Mario Tolvo <mariobros9@hotmail.it>
2024-08-19 12:42:48 +0200

    Array stampato al contrario

--- JsExercise/js_7/js.js
+++ JsExercise/js_7/js.js
@@ -1,12 +1,20 @@
 let array = [3,4,5,6,7,8,9,20];
 
-console.log(array);
+// console.log(array);
 
-let somma = 0;
+// let somma = 0;
 
-for(let i = 0; i < array.length; i++){
+// for(let i = 0; i < array.length; i++){
 
-    somma = array[i] + somma;
+//     somma = array[i] + somma;
+// }
+
+// console.log(somma);
+
+
+
+for(let i = array.length - 1; i >=0 ; i--){
+
+    console.log(array[i]);
+
 }
-
-console.log(somma);
