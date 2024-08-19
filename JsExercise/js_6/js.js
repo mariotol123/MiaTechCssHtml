85fb21c41de9c5e33e3deb206e512a376eb98464
Mario Tolvo <mariobros9@hotmail.it>
2024-08-19 10:33:44 +0200

    Switch case

--- JsExercise/js_6/js.js
+++ JsExercise/js_6/js.js
@@ -1,13 +1,38 @@
 let score = parseInt(prompt("Inserire un numero"));
 
-if(score >= 90 && score <= 100){
-    console.log("voto ottimo");
-} else if(score >= 70 && score <= 89){
-    console.log("voto buono");
-} else if (score >= 60 && score <= 69){
-    console.log("voto sufficiente");
-} else if (score <= 59){
-    console.log("voto insufficiente");
-} else {
-    console.log("punteggio non valido");
+// if(score >= 90 && score <= 100){
+//     console.log("voto ottimo");
+// } else if(score >= 70 && score <= 89){
+//     console.log("voto buono");
+// } else if (score >= 60 && score <= 69){
+//     console.log("voto sufficiente");
+// } else if (score <= 59){
+//     console.log("voto insufficiente");
+// } else {
+//     console.log("punteggio non valido");
+// }
+
+
+
+switch(true){
+
+    case isNaN(score):
+        console.log("voto non valido");
+        break;  
+
+    case score >= 90 && score <= 100:
+    console.log("Voto Ottimo");
+    break;
+
+    case score >= 70 && score <= 89:
+        console.log("Voto buono");
+        break;
+
+    case score >= 60 && score <= 69:
+        console.log("Voto sufficiente");
+        break;
+
+    case score <= 59:
+        console.log("Voto insufficiente");
+        break;  
 }
