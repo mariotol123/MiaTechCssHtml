40295a68d94f2bee590c65ed3ada9b383ce7a95a
Mario Tolvo <mariobros9@hotmail.it>
2024-08-19 18:15:54 +0200

    Object

---
+++ JsExercise/js_9/js.js
@@ -0,0 +1,17 @@
+let person = {
+    name : "Mario",
+    age : 27,
+    city : "Naples"
+}
+
+console.log(person);
+
+console.log(person.age);
+
+
+let personCopy = Object.assign({}, person);
+
+personCopy.age = 30;
+
+console.log(personCopy.age);
+console.log(person.age);
