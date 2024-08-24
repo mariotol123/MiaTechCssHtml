8dfd1840c2877f2a00f2baffeb82952c83861120
Mario Tolvo <mariobros9@hotmail.it>
2024-08-24 18:45:34 +0200

    Clousure

---
+++ JsExercise/js_10/js.js
@@ -0,0 +1,18 @@
+function outerFunction(x){
+
+    function innerFunction(y){
+
+        return x + y;
+
+    }
+
+    return innerFunction;
+
+};
+
+
+let firstFunction = outerFunction(5);
+
+let secondFunction = firstFunction(10);
+
+console.log(secondFunction);
