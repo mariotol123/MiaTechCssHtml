15624a2c18ca8a9705f3eadbc1860557d3d19b60
Mario Tolvo <mariobros9@hotmail.it>
2024-08-25 17:20:50 +0200

    Entries

--- JsExercise/js_12/js.js
+++ JsExercise/js_12/js.js
@@ -8,6 +8,18 @@ let student = {
 
 let key = Object.keys(student);
 let values = Object.values(student);
+let entries = Object.entries(student);
+
 
 console.log(key);
 console.log(values);
+console.log(entries);
+
+entries.forEach(element => {
+    
+    let chiave = element[0];
+    let valore = element[1];
+
+    console.log(`${chiave}: ${valore}`)
+
+});
