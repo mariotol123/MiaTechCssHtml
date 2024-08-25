9acb55a2e2d797fd88a399fc67f8e4b245571511
Mario Tolvo <mariobros9@hotmail.it>
2024-08-25 17:10:05 +0200

    Object.keys e object,values

---
+++ JsExercise/js_12/js.js
@@ -0,0 +1,13 @@
+let student = {
+    name : "Mario",
+    age : 27, 
+    school : "Sbordone",
+    grade : "Boh"
+}
+
+
+let key = Object.keys(student);
+let values = Object.values(student);
+
+console.log(key);
+console.log(values);
