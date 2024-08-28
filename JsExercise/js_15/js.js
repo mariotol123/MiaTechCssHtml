49f15b802ebfe42e69fc32b5cb72fe288b48051f
Mario Tolvo <mariobros9@hotmail.it>
2024-08-28 15:02:27 +0200

    Aggiunta di auto elettrica

--- JsExercise/js_15/js.js
+++ JsExercise/js_15/js.js
@@ -11,19 +11,20 @@ class Automobile {
         this.chilometraggio = chilometraggio;
     }
 
-    description(){
-        console.log(`La marca: ${this.marca} e' del modello ${this.modello} dell'anno ${this.anno}`);
-    }
-
+    
     aggiungiChilometraggio(number){
         
         this.chilometraggio = this.chilometraggio + number;
-
+        
     }
-
-     mostraChilometraggio(){
+    
+    mostraChilometraggio(){
         console.log(`${this.chilometraggio}`);
-     }
+    }
+    
+    description(){
+        console.log(`La marca: ${this.marca} e' del modello ${this.modello} dell'anno ${this.anno} e ${this.chilometraggio}`);
+    }
 
 }
 
@@ -34,3 +35,38 @@ auto.description();
 
 auto.aggiungiChilometraggio(1);
  auto.mostraChilometraggio();
+
+
+
+
+
+ class Elettrica extends Automobile {
+    autonomia;
+
+    constructor(marca,modello,anno,chilometraggio,autonomia){
+        super(marca,modello,anno,chilometraggio);
+        this.autonomia = autonomia;
+    }
+
+    description(){
+
+        console.log(`La marca: ${this.marca} e' del modello ${this.modello} dell'anno ${this.anno} e ${this.chilometraggio} in piu ha l'autonomia di ${this.autonomia} km`);
+
+    }
+
+    ricarica(km){
+        this.autonomia += km;
+    }
+
+    mostraRicarica(){
+        console.log(`${this.autonomia}`);
+    }
+
+ }
+
+ let autoElettrica = new Elettrica("MErcedes", "GLS", 2024, 150000, 300);
+
+ autoElettrica.description();
+
+ autoElettrica.ricarica(100);
+ autoElettrica.mostraRicarica();
