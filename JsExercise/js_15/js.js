7cfccc92c7013fc1fa31a9063ac359dc70fdd90e
Mario Tolvo <mariobros9@hotmail.it>
2024-08-28 14:55:32 +0200

    Metodi chilometraggio

--- JsExercise/js_15/js.js
+++ JsExercise/js_15/js.js
@@ -2,19 +2,35 @@ class Automobile {
     marca;
     modello;
     anno;
+    chilometraggio;
 
-    constructor(marca,modello,anno){
+    constructor(marca,modello,anno,chilometraggio){
         this.marca = marca;
         this.modello = modello;
         this.anno = anno;
+        this.chilometraggio = chilometraggio;
     }
 
     description(){
         console.log(`La marca: ${this.marca} e' del modello ${this.modello} dell'anno ${this.anno}`);
     }
+
+    aggiungiChilometraggio(number){
+        
+        this.chilometraggio = this.chilometraggio + number;
+
+    }
+
+     mostraChilometraggio(){
+        console.log(`${this.chilometraggio}`);
+     }
+
 }
 
 
-let auto = new Automobile("Renault", "C4", 1988);
+let auto = new Automobile("Renault", "C4", 1988,200000);
 
 auto.description();
+
+auto.aggiungiChilometraggio(1);
+ auto.mostraChilometraggio();
