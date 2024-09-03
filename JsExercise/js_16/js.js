7babe2d3ea2e1788bb446f21e6ce5d0c25236d19
Mario Tolvo <mariobros9@hotmail.it>
2024-09-03 10:57:59 +0200

    Destructuring con valori predefiniti

--- JsExercise/js_16/js.js
+++ JsExercise/js_16/js.js
@@ -12,3 +12,14 @@ let [uno, , tre] = numbers2;
 
 console.log(uno);
 console.log(tre);
+
+
+let mario = ["Mario", "Rossi", "Panettiere"];
+
+let [nome, cognome, mansione, eta = 25, citta = "Napoli"] = mario;
+
+console.log(nome);
+console.log(cognome);
+console.log(mansione);
+console.log(eta);
+console.log(citta);
