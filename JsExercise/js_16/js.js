5b333563779e07142c45d7af89fa02d99349740d
Mario Tolvo <mariobros9@hotmail.it>
2024-09-03 10:52:06 +0200

    Destructuring saltando elementi

--- JsExercise/js_16/js.js
+++ JsExercise/js_16/js.js
@@ -4,3 +4,11 @@ let [numeroUno, numeroDue] = numbers;
 
 console.log(numeroUno);
 console.log(numeroDue);
+
+
+let numbers2 = [1,2,3];
+
+let [uno, , tre] = numbers2;
+
+console.log(uno);
+console.log(tre);
