7f37cf82dd801155711f01c71badb00c304d01f1
Mario Tolvo <mariobros9@hotmail.it>
2024-09-04 15:22:21 +0200

    Uniamo due array con lo spread operator

--- JsExercise/js_17/js.js
+++ JsExercise/js_17/js.js
@@ -9,3 +9,12 @@ array = [10,20,30];
 
 console.log(array);
 console.log(newArray);
+
+
+
+let arrayNumbers = [100,200,300];
+let arrayNumbers2 = [500,600,700];
+
+
+let joinArray = [...arrayNumbers,...arrayNumbers2];
+console.log(joinArray);
