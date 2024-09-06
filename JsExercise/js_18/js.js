e1ac25760b0da30183a20fdf93364053b42c0ae0
Mario Tolvo <mariobros9@hotmail.it>
2024-09-06 10:53:01 +0200

    Template literals

---
+++ JsExercise/js_18/js.js
@@ -0,0 +1,12 @@
+let nome = "Mario";
+let cognome = "Tolvo";
+let eta = 27;
+let citta = "Napoli";
+
+console.log(`Il mio nome e' ${nome} e il mio cognome e' ${cognome}`);
+
+console.log(`
+    Il mio nome e' ${nome} e il mio cognome e' ${cognome}
+    vivo a ${citta}
+    e ho ${eta}
+    `);
