db60c50c2a75ff75ced93c06ea8cd650c1dea125
Mario Tolvo <mariobros9@hotmail.it>
2024-09-06 12:21:04 +0200

    SetInterval bloccato

--- JsExercise/js_19/js.js
+++ JsExercise/js_19/js.js
@@ -1,6 +1,16 @@
 
 
     document.write("Contiamo i secondi: </br>");
-    setInterval(function () {
+    let intervalId = setInterval(function () {
         document.body.innerHTML += ("Sono passati 2 secondi</br>");
     }, 2000);
+
+
+
+    setTimeout(function () {
+
+        clearInterval(intervalId);
+
+        document.body.innerHTML += ("Intervallo stoppato dopo 10 secondi")
+
+    }, 10000);
