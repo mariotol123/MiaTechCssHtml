6a66e55dc9f40a20c36bf0b4db16f9897d0aaa23
Mario Tolvo <mariobros9@hotmail.it>
2024-09-25 12:11:54 +0200

    Nuovi colori aggiunti alla config

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+         "nuovo-colore" : "#ff5733",
+         "altro-colore" : "#33c1ff",
+      },
+    },
+  },
+  plugins: [],
+}
+
