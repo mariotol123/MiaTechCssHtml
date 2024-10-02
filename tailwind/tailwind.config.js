a33978c2e00c3568c3cdefcf76a1c0c51fa31bc0
Mario Tolvo <mariobros9@hotmail.it>
2024-10-02 11:13:25 +0200

    colori di testo

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -9,6 +9,22 @@ module.exports = {
       colors: {
         "nuovo-colore": "#ff5733", // Colore personalizzato
         "altro-colore": "#33c1ff",
+
+        primary: {
+          light: '#93C5FD', // Celeste chiaro
+          DEFAULT: '#3B82F6', // Colore blu primario
+          dark: '#1E40AF', // Blu scuro
+        },
+        secondary: {
+          light: '#FDE68A', // Giallo chiaro
+          DEFAULT: '#F59E0B', // Colore secondario giallo
+          dark: '#B45309', // Giallo scuro
+        },
+        accent: {
+          light: '#A7F3D0', // Verde chiaro
+          DEFAULT: '#10B981', // Colore verde
+          dark: '#047857', // Verde scuro
+        },
       },
       padding: {
         '5/6': '83.3333%', // Nuova dimensione di padding
