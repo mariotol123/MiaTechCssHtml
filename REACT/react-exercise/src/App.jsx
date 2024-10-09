ffe8bba37ffbdf054c608ec3c5206d8c05968f0c
Mario Tolvo <mariobros9@hotmail.it>
2024-10-09 18:02:51 +0200

    props.children

--- REACT/react-exercise/src/App.jsx
+++ REACT/react-exercise/src/App.jsx
@@ -4,6 +4,7 @@ import Counter from './Counter'
 import LoginForm from './LoginForm'
 import UncontrolledInput from './UncrontrolledInput'
 import ItemList from './ItemList'
+import Card from './Card'
 
 const charArray = ['A', 'B', 'C', 'D'];
 
@@ -20,6 +21,22 @@ function App() {
       <br />
       <br />
       <ItemList array={charArray} />
+      <br /><br /><br />
+      <div>
+            <h1>My Cards</h1>
+            <Card>
+                <h2>Card Title 1</h2>
+                <p> card 1.</p>
+            </Card>
+            <Card>
+                <h2>Card Title 2</h2>
+                <p> card 2.</p>
+            </Card>
+            <Card>
+                <h2>Card Title 3</h2>
+                <p> card 3.</p>
+            </Card>
+        </div>
     </>
   )
 }
