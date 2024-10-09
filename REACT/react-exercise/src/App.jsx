ad5ff59e726829310f4044c5b9c7b6997d653578
Mario Tolvo <mariobros9@hotmail.it>
2024-10-09 16:38:21 +0200

    useRef

--- REACT/react-exercise/src/App.jsx
+++ REACT/react-exercise/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import HelloWorld from './HelloWorld'
 import Counter from './Counter'
 import LoginForm from './LoginForm'
+import UncontrolledInput from './UncrontrolledInput'
 
 function App() {
   return (
@@ -9,6 +10,9 @@ function App() {
       <HelloWorld />
       <Counter />
       <LoginForm />
+      <br />
+      <br />
+      <UncontrolledInput />
     </>
   )
 }
--- REACT/react-exercise/src/UncrontrolledInput.jsx
+++ REACT/react-exercise/src/UncrontrolledInput.jsx
@@ -1,10 +1,17 @@
 import { useRef } from "react";
 
 function UncontrolledInput() {
-   
+    
+    const inputRef = useRef(null);
+
+    const handleShowValue = () => {
+        alert(`Input value: ${inputRef.current.value}`);
+    };
+
     return (
         <div>
-            <input type="text" />
+            <input type="text" ref={inputRef} />
+            <button onClick={handleShowValue}>Mostra Input</button>
         </div>
     );
 }
