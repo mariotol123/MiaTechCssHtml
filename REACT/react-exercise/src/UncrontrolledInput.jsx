a035d85815f67ade493700d703e0ae7ea297dd04
Mario Tolvo <mariobros9@hotmail.it>
2024-10-09 16:34:06 +0200

    UncontrolledInput

---
+++ REACT/react-exercise/src/UncrontrolledInput.jsx
@@ -0,0 +1,12 @@
+import { useRef } from "react";
+
+function UncontrolledInput() {
+   
+    return (
+        <div>
+            <input type="text" />
+        </div>
+    );
+}
+
+export default UncontrolledInput;
