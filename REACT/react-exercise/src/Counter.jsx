f67676aa529d3a6c088735b5d76a91de4b7beb31
Mario Tolvo <mariobros9@hotmail.it>
2024-10-09 15:56:27 +0200

    useEffect

--- REACT/react-exercise/src/Counter.jsx
+++ REACT/react-exercise/src/Counter.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
+import { useEffect } from "react";
 
 function Counter() {
 
     const [counter, setCounter] = useState(0);
+    const [h2Text, setH2Text] = useState("Counter is 0")
 
     const incrementClickButton = () => {
         setCounter((counter) => {
@@ -20,9 +22,13 @@ function Counter() {
         setCounter(0);
     }
 
+    useEffect (() => {
+        setH2Text(`Il counter e' ${counter}`)
+    }, [counter])
 
     return (
         <div>
+            <h2>{h2Text}</h2>
             <button onClick={incrementClickButton}>ADD</button>
             <button onClick={decrementClickButton}>REMOVE</button>
             <button onClick={resetClickButton}>RESET</button>
