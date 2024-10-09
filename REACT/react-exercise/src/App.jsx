9d03e97f6871a6ca43122924316f7ab29b99113f
Mario Tolvo <mariobros9@hotmail.it>
2024-10-09 16:13:32 +0200

    LoginForm

--- REACT/react-exercise/src/App.jsx
+++ REACT/react-exercise/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import HelloWorld from './HelloWorld'
 import Counter from './Counter'
+import LoginForm from './LoginForm'
 
 function App() {
   return (
     <>
       <HelloWorld />
       <Counter />
+      <LoginForm />
     </>
   )
 }
--- REACT/react-exercise/src/LoginForm.jsx
+++ REACT/react-exercise/src/LoginForm.jsx
@@ -3,14 +3,23 @@ import { useState } from "react";
 function LoginForm () {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        alert(`Username: ${username}, Password: ${password}`);
+    }
+
     return (
         <>
             <div>
                 <form action="">
-                    <input type="text" name="username" value={username} />
-                    <input type="text" name="password" value={password} />
+                    <input type="text" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                    <input type="text" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <button type="submit" onClick={handleSubmit}>PIGIAAA</button>
                 </form>
             </div>
         </>
     )
 }
+
+export default LoginForm;
