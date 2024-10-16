863ac0046e205ca4c382ac4438b7df9a6e8ac82e
Mario Tolvo <mariobros9@hotmail.it>
2024-10-16 10:11:38 +0200

    useFilteredTodos

--- REACT/exercise-2/src/components/TodoList.jsx
+++ REACT/exercise-2/src/components/TodoList.jsx
@@ -1,9 +1,13 @@
 import useFetch from "../hooks/useFetch";
+import { useState } from "react";
+import useFilteredTodos from "../hooks/useFilteredTodos";
 import React from "react";
 
 
 function TodoList () {
     const {data,error,loading} = useFetch("https://jsonplaceholder.typicode.com/todos");
+    const [searchTerm, setSearchTerm] = useState(''); 
+    const filteredTodos = useFilteredTodos(data, searchTerm);
 
     if (loading) return <p>Loading todos...</p>;
     if(error) return <p>{error}</p>
@@ -12,8 +16,14 @@ function TodoList () {
     return (
         <div>
             <h1>Todo List...</h1>
+            { <input 
+                type="text" 
+                placeholder="RIcerca todos..." 
+                value={searchTerm}
+                onChange={e => setSearchTerm(e.target.value)}
+            />}
             <ul>
-                {data.map((todo) => (
+                {filteredTodos.map((todo) => (
                     <li key={todo.id}>
                     {todo.title} {todo.completed ? "✅" : "❌"}
                 </li>
