83d4d33c1815cf568ed487c6a4dd998abfb2b34b
Mario Tolvo <mariobros9@hotmail.it>
2024-10-18 12:17:06 +0200

    useRef

--- REACT/exercise-2/src/components/TodoList.jsx
+++ REACT/exercise-2/src/components/TodoList.jsx
@@ -1,5 +1,5 @@
 import useFetch from "../hooks/useFetch";
-import { useState, useCallback, useMemo } from "react";
+import { useState, useCallback, useMemo, useRef, useEffect } from "react";
 import useFilteredTodos from "../hooks/useFilteredTodos";
 import React from "react";
 
@@ -7,10 +7,22 @@ import React from "react";
 function TodoList () {
     const {data,error,loading} = useFetch("https://jsonplaceholder.typicode.com/todos");
     const [searchTerm, setSearchTerm] = useState(''); 
-    const filteredTodos = useMemo(() => useFilteredTodos(data, searchTerm)) ;
+    const filteredTodos = useFilteredTodos(data, searchTerm);
+    const memoizedFilteredTodos = useMemo(() => filteredTodos, [filteredTodos]);
+    const inputRef = useRef();
 
     const handleSearchChange = useCallback((e) => {
         setSearchTerm(e.target.value);
+    }, []);
+
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            if (inputRef.current) {
+                inputRef.current.focus();
+            }
+        }, 100); //Non mi funzionava perche useEffect veniva eseguito prima del montaggio di input.
+
     }, []);
 
     if (loading) return <p>Loading todos...</p>;
@@ -21,13 +33,14 @@ function TodoList () {
         <div>
             <h1>Todo List...</h1>
             { <input 
+                ref={inputRef}
                 type="text" 
                 placeholder="RIcerca todos..." 
                 value={searchTerm}
                 onChange={handleSearchChange}
             />}
             <ul>
-                {filteredTodos.map((todo) => (
+                {memoizedFilteredTodos.map((todo) => (
                     <li key={todo.id}>
                     {todo.title} {todo.completed ? "✅" : "❌"}
                 </li>
