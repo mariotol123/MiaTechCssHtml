70be2422c092ebfe35ed3918b731aef631dde77b
Mario Tolvo <mariobros9@hotmail.it>
2024-10-24 16:48:36 +0200

    useNavigate

--- REACT/exercise-2/src/components/About.jsx
+++ REACT/exercise-2/src/components/About.jsx
@@ -1,8 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function About () {
+    const navigate = useNavigate();
+
+    const handleHome = () => {
+        navigate("/")
+    }
+
     return (
+        <>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure consequuntur laborum dignissimos incidunt deserunt. Eum, quos. Ad tenetur enim, nesciunt perferendis facilis id voluptate consequuntur soluta debitis magni autem at!</p>
+
+        <button onClick={handleHome}>
+            Clicca per ritornare alla home!!!
+        </button>
+        </>
     )
 }
 
